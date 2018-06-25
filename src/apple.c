8593f575819644eb661d99430324dfac65262d29
Kawaguchi Yuma <kawaguchi.y.ac@is-mac11.edu.c.titech.ac.jp>
2018-06-25 17:48:59 +0900

    課題

--- src/apple.c
+++ src/apple.c
@@ -5,13 +5,56 @@ int k;
 int A[100000];
 
 
+int bag(int m){
+    int i, a, b;
+    a = 0;
+    b = 0;
+    for(i = 0; i < n; i++){
+        if(A[i]%m == 0){
+            b = a + A[i]/m;
+        }
+        else {
+            b = a + A[i]/m + 1;
+        }
+        a = b;
+    }
+    return a;
+}
+
+int bagmax(){
+    int i, a, b;
+    a = 0;
+    for(i = 0; i < n; i++){
+        b = a + A[i];
+        a = b;
+    }
+    return a;
+}
+
+int p(int m){
+    return (int) bag(m) <= k;
+}
+
+
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = bagmax();
+    while(ub-lb > 1){
+        int m = (lb+ub)/2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    
+    
+    printf("%d\n", ub);
+    return 0;
 }
